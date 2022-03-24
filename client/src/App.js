a455a1435e5ab9b078c8d6f2875e37e74fa9a575
Mason Wallis <mwallis5110@gmail.com>
2022-03-24 09:10:09 -0500

    Built initial pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from "./pages/Home";
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
